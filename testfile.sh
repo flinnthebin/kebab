dd95247fbf1824282c545fcc596965d6cb5008ba
Chris Flinn <christopher.flinn@gmail.com>
2023-05-07 09:20:03 +1000

    fix testscript so chars have whitespace and are identifiable as independent words, fix bug with backup nesting into first renamed directory

--- testfile.sh
+++ testfile.sh
@@ -2,4 +2,4 @@
 
 # create a series of folders 1-10 with a series of text files 1-10 populated with between 1-10 x chars
 
-for i in {1..10}; do mkdir -p "DIR$i" && echo -n $(printf "%0.sx" $(seq 1 $i)) > "DIR$i/FILE$i" && cd "DIR$i"; done
+for i in {1..10}; do mkdir -p "DIR$i" && printf "%0.sx " $(seq 1 $i) > "DIR$i/FILE$i" && cd "DIR$i"; done
